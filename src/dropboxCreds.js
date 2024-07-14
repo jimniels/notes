1fd34c5ac9aaa6390c3d4ed2797566ff60221c5b
Jim Nielsen <jimniels@gmail.com>
2024-07-13 23:04:02 -0600

    more tweaks

---
+++ src/dropboxCreds.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import process from "node:process";
+
+// Offer access to environment variables after loading .env file.
+export default () =>
+  JSON.stringify({
+    app_key: process.env.DBX_APP_KEY,
+    app_secret: process.env.DBX_APP_SECRET,
+    refresh_token: process.env.DBX_REFRESH_TOKEN,
+  });
