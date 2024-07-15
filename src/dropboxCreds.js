2c74d40c8151cae84c07ebd014d0f219e82f405d
Jim Nielsen <jimniels@gmail.com>
2024-07-15 12:33:52 -0600

    change how i pipe in creds

--- src/dropboxCreds.js
+++ src/dropboxCreds.js
@@ -2,9 +2,8 @@ import "dotenv/config";
 import process from "node:process";
 
 // Offer access to environment variables after loading .env file.
-export default () =>
-  JSON.stringify({
-    app_key: process.env.DBX_APP_KEY,
-    app_secret: process.env.DBX_APP_SECRET,
-    refresh_token: process.env.DBX_REFRESH_TOKEN,
-  });
+export default () => ({
+  app_key: process.env.DBX_APP_KEY,
+  app_secret: process.env.DBX_APP_SECRET,
+  refresh_token: process.env.DBX_REFRESH_TOKEN,
+});
